d76cdfa3bec1e3a10c57976011aa9552c28ab8fc
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-15 15:12:48 +0100

    generated models and db tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_134526) do
+ActiveRecord::Schema.define(version: 2020_08_15_141120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "job_id", null: false
+    t.string "invoice"
+    t.datetime "confirmed_datetime"
+    t.boolean "completed"
+    t.boolean "confirmed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_bookings_on_job_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.string "title"
+    t.string "description"
+    t.string "address"
+    t.datetime "due_date"
+    t.integer "start_time"
+    t.integer "end_time"
+    t.float "longitude"
+    t.float "latitude"
+    t.float "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_jobs_on_category_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "booking_id", null: false
+    t.integer "rating"
+    t.string "title"
+    t.string "description"
+    t.boolean "tasker"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +76,9 @@ ActiveRecord::Schema.define(version: 2020_08_15_134526) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "jobs"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "jobs", "categories"
+  add_foreign_key "jobs", "users"
+  add_foreign_key "reviews", "bookings"
 end
