3e0e08e2714dd366f3bc7abdb6fc8664d296983a
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-18 21:15:30 +0100

    working on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,4 @@ class User < ApplicationRecord
   has_many :jobs
   has_many :bookings
   has_many :reviews, through: :bookings
-  has_many :job_bookings, through: :jobs, class_name: :Booking, foregin_key: :job_id
-  has_many :job_reviews, through: :job_bookings, class_name: :Review, foreign_key: :booking_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_192212) do
+ActiveRecord::Schema.define(version: 2020_08_18_201224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_192212) do
     t.boolean "tasker"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
