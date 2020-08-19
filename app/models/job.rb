27b742986381a1723c703106b9126d02a669134e
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-19 08:57:28 +0100

    added first draft of associations and added user_category model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,6 +2,5 @@ class Job < ApplicationRecord
   # This user is the person who created the job
   belongs_to :user
   belongs_to :category
-  belongs_to :booking
-  has_many :reviews, through: :bookings, as :reviewable
+  has_many :bookings
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,5 @@
 class Review < ApplicationRecord
   # belongs_to :reviewable, polymorphic: true
   belongs_to :booking
+  belongs_to :user
 end
