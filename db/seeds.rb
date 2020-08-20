35fc2b3791123e99b550ad156d1e7ff0d74d3525
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-20 20:46:20 +0100

    added working seeds for necessary tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts 'creating users...'
 bob = User.create(
   {
     first_name: 'Bob',
@@ -14,8 +15,10 @@ bob = User.create(
     email: 'bob@me.com',
     password: 'secure_pass5!',
     password_confirmation: 'secure_pass5!'
-    }
+  }
 )
+puts "created #{bob}"
+
 sam = User.create(
   {
     first_name: 'Sam',
@@ -26,7 +29,61 @@ sam = User.create(
     password_confirmation: 'secure_pass5!'
   }
 )
+puts "created #{sam}"
 
-job1 = Job.create({ title: 'Need groceries', description: 'food pls', user_id: 1 })
+puts 'creating categories...'
+groceries = Category.create(
+  {
+    name: 'Groceries'
+  }
+)
+puts "created #{groceries}"
 
-booking1 = Booking.create({user_id: 2, job_id: 1})
+medicine = Category.create(
+  {
+    name: 'Medicine'
+  }
+)
+puts "created #{medicine}"
+
+puts 'creating jobs...'
+job1 = Job.create(
+  {
+    title: 'Need groceries',
+    description: 'food pls',
+    category_id: 1,
+    user_id: 1
+  }
+)
+puts "created #{job1}"
+
+job2 = Job.create(
+  {
+    title: 'Need medicien',
+    description: 'medicine pls',
+    category_id: 2,
+    user_id: 1
+  }
+)
+puts "created #{job2}"
+
+puts 'creating bookings...'
+booking1 = Booking.create(
+  {
+    user_id: 2,
+    job_id: 1,
+    confirmed: false,
+    completed: false
+  }
+)
+puts "created #{booking1}"
+
+booking2 = Booking.create(
+  {
+    user_id: 2,
+    job_id: 2,
+    confirmed: true,
+    completed: false
+  }
+)
+puts "created #{booking2}"
