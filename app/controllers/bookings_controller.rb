1b162ec5c403fb1c30690e457142b7defafefa59
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-20 21:29:45 +0100

    added basic index page for bookings.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,14 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_boooking, only: :show
 
   def index
     @bookings = Booking.all
   end
 
-  def show
+  def show; end
+
+  def set_booking
     @booking = Booking.find(params[:id])
   end
 end
