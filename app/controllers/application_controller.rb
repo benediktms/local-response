15e0bc092660fd5c9a384efda7877ef3b50ff79c
Nikki Booth <61873199+boothnation@users.noreply.github.com>
2020-08-22 14:16:18 +0100

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def configure_devise_permitted_parameters
+  def configure_permitted_parameters
     registration_params = [:first_name, :last_name, :address, :email, :password, :password_confirmation]
 
     if params[:action] == 'update'
