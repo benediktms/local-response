6651b31f56d525623e26cbce60d4d90374e3e707
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-08-25 11:04:44 +0100

    added geocoding fucntionality and map rendering

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,10 +26,6 @@ require("channels")
 import "bootstrap";
 import { initMapbox } from "../plugins/init_mapbox";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
-
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
   initMapbox();
 });
