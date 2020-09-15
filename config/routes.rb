e16559b6ee83aee0fbade689d37c8ee8e998265c
Nikki Booth <nikabooth@me.com>
2020-09-15 20:47:47 +0100

    small cahnges

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   end
   resources :chatrooms, only: :show do
     resources :messages, only: :create
-  # end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
