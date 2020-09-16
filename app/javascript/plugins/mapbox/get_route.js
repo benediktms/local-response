b219b080b64ddd6e9c1098b97b909a37ebcc1ac8
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-09-16 08:07:41 +0100

    moving mapbox geocoder style import to document head

--- app/javascript/plugins/mapbox/get_route.js
+++ app/javascript/plugins/mapbox/get_route.js
@@ -18,11 +18,9 @@ export const getRoute = async (map, coordinates, apiKey) => {
   } else {
     // if the route already exists on the map, reset it using setData
     if (map.getSource('route')) {
-      console.log('set route data');
       map.getSource('route').setData(route);
     } else {
       // otherwise, make a new request
-      console.log('adding route map layer');
       map.addLayer({
         id: 'route',
         type: 'line',
@@ -50,10 +48,8 @@ export const getRoute = async (map, coordinates, apiKey) => {
     }
 
     if (map.getLayer('start')) {
-      console.log('set data start');
       map.getSource('start').setData(startCoords);
     } else {
-      console.log('adding start point map layer');
       map.addLayer({
         id: 'start',
         type: 'circle',
@@ -81,10 +77,8 @@ export const getRoute = async (map, coordinates, apiKey) => {
     }
 
     if (map.getLayer('end')) {
-      console.log('set data end');
       map.getSource('end').setData(endCoords);
     } else {
-      console.log('adding end point map layer');
       map.addLayer({
         id: 'end',
         type: 'circle',
