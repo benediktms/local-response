cd2515a862fab81c783d8ce42e2f2ad91a53491d
Benedikt Schnatterbeck <benedikt.schnatterbeck@gmail.com>
2020-09-22 21:11:12 +0100

    adding condition to zoom map out if only one marker is visible

--- app/javascript/plugins/mapbox/fit_markers.js
+++ app/javascript/plugins/mapbox/fit_markers.js
@@ -4,6 +4,15 @@ export const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
 
   markers.forEach((marker) => bounds.extend([marker.long, marker.lat]));
-  map.fitBounds(bounds, { padding: 70, duration: 0 });
+  map.fitBounds(bounds, {
+    padding: 70,
+    duration: 0,
+  });
+
+  if (markers.length === 1) {
+    map.fitBounds(bounds, {
+      zoom: 15,
+    });
+  }
   // console.log(bounds);
 };
